165a44b36e827f6a4f980e603523869bf5dcd2bf
Deepak Guptha <vzan2012@gmail.com>
2019-12-12 23:39:56 +0530

    feat(scripts): Updation - fat arrow functions

--- examples-2.js
+++ examples-2.js
@@ -27,3 +27,71 @@ var google = new website('http://google.com', 123456);
 // google.url = 'http://google.com';
 // google.facebookLikes = 123456;
 console.log(google.likesInK());
+// Getter - Example
+// class rectangle {
+//     l1: number;
+//     l2: number;
+//     constructor(l1: number, l2: number) {
+//         this.l1 = l1;
+//         this.l2 = l2;
+//     }
+//     get area():number {
+//         return this.l1 * this.l2;
+//     }
+// }
+// let myRect = new rectangle(10,20);
+// console.log(myRect.area);
+// Setter - Example 
+var rectangle = /** @class */ (function () {
+    function rectangle() {
+        console.log('Hello Rectangle');
+    }
+    Object.defineProperty(rectangle.prototype, "perimeter", {
+        set: function (length) {
+            this._perimeter = length;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(rectangle.prototype, "oneside", {
+        set: function (side) {
+            this.side1 = side;
+            this.side2 = (this._perimeter - 2 * this.side1) / 2;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(rectangle.prototype, "area", {
+        get: function () {
+            return this.side1 * this.side2;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    rectangle.prototype.printAllInfo = function () {
+        console.log("Perimeter: " + this._perimeter + ", Side One: " + this.side1 + ", Side Two: " + this.side2);
+    };
+    return rectangle;
+}());
+var myRect = new rectangle();
+myRect.perimeter = 10;
+myRect.oneside = 2.5;
+console.log(myRect.area);
+myRect.printAllInfo();
+// Fat Arrow Functions - Examples 
+var car = /** @class */ (function () {
+    function car() {
+        this.gasAmount = 0;
+        this.carObj = document.querySelector('#playerCar');
+    }
+    car.prototype.run = function () {
+        var _this = this;
+        setInterval(function () {
+            _this.gasAmount++;
+            _this.carObj.innerText = _this.gasAmount.toString();
+        }, 500);
+    };
+    return car;
+}());
+var myCar = new car();
+myCar.run();
--- examples-2.ts
+++ examples-2.ts
@@ -60,3 +60,84 @@ let google = new website('http://google.com', 123456);
 // google.url = 'http://google.com';
 // google.facebookLikes = 123456;
 console.log(google.likesInK());
+
+
+
+// Getter - Example
+// class rectangle {
+//     l1: number;
+//     l2: number;
+
+//     constructor(l1: number, l2: number) {
+//         this.l1 = l1;
+//         this.l2 = l2;
+//     }
+
+//     get area():number {
+//         return this.l1 * this.l2;
+//     }
+// }
+
+// let myRect = new rectangle(10,20);
+// console.log(myRect.area);
+
+
+
+// Setter - Example 
+class rectangle {
+
+    _perimeter: number;
+    side1: number;
+    side2: number;
+
+    constructor() {
+        console.log('Hello Rectangle');
+    }
+
+    set perimeter(length: number) {
+        this._perimeter = length;
+    }
+
+    set oneside(side: number) {
+        this.side1 = side;
+        this.side2 = (this._perimeter - 2 * this.side1) / 2;
+    }
+
+    get area(): number {
+        return this.side1 * this.side2;
+    }
+
+    printAllInfo(): any {
+        console.log(`Perimeter: ${this._perimeter}, Side One: ${this.side1}, Side Two: ${this.side2}`);
+    }
+}
+
+let myRect = new rectangle();
+myRect.perimeter = 10;
+myRect.oneside = 2.5;
+console.log(myRect.area);
+myRect.printAllInfo();
+
+
+
+// Fat Arrow Functions - Examples 
+class car {
+    gasAmount: number;
+    carObj: HTMLElement;
+
+    constructor() {
+        this.gasAmount = 0;
+        this.carObj = document.querySelector('#playerCar')
+    }
+
+    run(): void {
+        setInterval(() => {
+            this.gasAmount++;
+            this.carObj.innerText = this.gasAmount.toString();
+        }, 500);
+
+    }
+}
+
+let myCar = new car();
+myCar.run();
